286dbed56f6ede8b29b9ce78ea149f6b24e18a3e
Doug Washington <washide@gmail.com>
2020-01-24 01:00:01 -0500

    Added redirects for stdout/stderr

--- scripts/checkout_students.sh
+++ scripts/checkout_students.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 [ ! -d ~/output ] && mkdir output
-sqlite3 TimeTrack4237.db < scripts/checkout_students.sql > output/checkout_students.out
+exec > output/checkout_students_$(date +%Y_%m_%d).out                                                                      
+exec 2>&1
+/usr/bin/sqlite3 TimeTrack4237.db < scripts/checkout_students.sql
