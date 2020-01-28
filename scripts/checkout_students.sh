18dbee2f2c9e456f72fe5c0465f6ab57d09db02d
Doug Washington <washide@gmail.com>
2020-01-27 20:43:10 -0500

    Updated scripts to call them from the correct directory

--- scripts/checkout_students.sh
+++ scripts/checkout_students.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-[ ! -d ~/output ] && mkdir output
+cd ~/TimeTrack4237
+[ ! -d output ] && mkdir output
 exec > output/checkout_students_$(date +%Y_%m_%d).out                                                                      
 exec 2>&1
 /usr/bin/sqlite3 TimeTrack4237.db < scripts/checkout_students.sql
