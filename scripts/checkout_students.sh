2f029fd3b01f03c163843fd3f22490af492164cf
Doug Washington <washide@gmail.com>
2020-01-28 19:36:02 -0500

    Removed whitespaces

--- scripts/checkout_students.sh
+++ scripts/checkout_students.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ~/TimeTrack4237
 [ ! -d output ] && mkdir output
-exec > output/checkout_students_$(date +%Y_%m_%d).out                                                                      
+exec > output/checkout_students_$(date +%Y_%m_%d).out
 exec 2>&1
 /usr/bin/sqlite3 TimeTrack4237.db < scripts/checkout_students.sql
